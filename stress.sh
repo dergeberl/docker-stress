d7d28312256655dab326b4577e47984dbb1a1d42
dergeberl <github@dergeberl.de>
2020-05-06 16:46:18 +0200

    add sleep var

--- stress.sh
+++ stress.sh
@@ -4,6 +4,7 @@ do
 [ -z "$TIMEOUT" ] && TIMEOUT=30
 [ -z "$CPU" ] && CPU=1
 [ -z "$LOAD" ] && LOAD=50
+[ -z "$SLEEP" ] && SLEEP=5
 stress-ng -c $CPU -l $LOAD --timeout $TIMEOUT &
-sleep 2
+sleep $SLEEP
 done
